ee0a08e644ce83fd819eb032f4c1b515c4905475
Stephen <stevOOOOOOOOOO0@YAHOO.COM>
2019-09-15 21:41:13 -0600

    Created the Scene and prototyped the enemy movement

---
+++ Tower Defense/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+	public Transform finalDestination;
+	public UnityEngine.AI.NavMeshAgent agent;
+	public FloatData PlaySpeed;
+	
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(finalDestination.position);
+		agent.speed = PlaySpeed.Value;
+	}
+
+	public void ChangeSpeed()
+	{
+		agent.speed = PlaySpeed.Value;
+	}
+}
---
+++ Tower Defense/Assets/Scripts/FloatData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+
+	public float Value;
+
+	public void DoubleValue()
+	{
+		Value = Value * 2;
+	}
+
+	public void HalfValue()
+	{
+		Value = Value / 2;
+	}
+}
