712bdf45c9cc7b478dc614930633f65387c6705d
514 Classroom <stevOOOOOOOOOO0@yahoo.com>
2019-10-02 14:48:11 -0600

    worked on code for bullet creation.
    not quite done

--- Tower Defense/Assets/Scripts/Behaviours.cs
+++ Tower Defense/Assets/Scripts/Behaviours.cs
@@ -1,37 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Behaviours : MonoBehaviour
 {
 
+	public float WaitTime;
+	public bool Runner = true;
 	public float Smooth = 1f;
-	public List<Vector3> Targets;
+	public List<Transform> Targets;
 	private Rigidbody Character;
+	public UnityEvent StayAction, TriggerEnterAction, TriggerExitAction;
+	public GameObject newObject;
 
 	public void Start()
 	{
 		Character = GetComponent<Rigidbody>();
 	}
 	
-	public void FaceTowards(Vector3 Target)
+	public void FaceTowards(Transform Target)
 	{
-		Character.MoveRotation(Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Target), Smooth));
+		Character.MoveRotation(Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Target.position), Smooth));
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		Targets.Add(other.transform.position);
+		Targets.Add(other.transform);
+		TriggerEnterAction.Invoke();
 	}
 	
-	private void OnTriggerStay(Collider other)
+	/*private void OnTriggerStay(Collider other)
 	{
 		FaceTowards(Targets[0]);
-	}
+		StayAction.Invoke();
+	}*/
 
 	private void OnTriggerExit(Collider other)
 	{
-		Targets.Remove(other.transform.position);
+		Targets.Remove(other.transform);
+		TriggerExitAction.Invoke();
+	}
+
+	public void RunCoroutine()
+	{
+		StartCoroutine(BulletCreation());
+	}
+
+	public void StopCoroutine()
+	{
+		Runner = false;
+	}
+	
+	public IEnumerator BulletCreation () {
+		while (Runner)
+		{
+			Instantiate(newObject);
+			yield return new WaitForSeconds(WaitTime);
+		}
 	}
 }
---
+++ Tower Defense/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+	
+	
+	public bool Runner = false;
+	
+	// Use this for initialization
+	public IEnumerator OnStart () {
+		while (!Runner)
+		{
+			
+			yield return new WaitForSeconds(2);
+		}
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision other)
+	{
+		Runner = false;
+	}
+}
