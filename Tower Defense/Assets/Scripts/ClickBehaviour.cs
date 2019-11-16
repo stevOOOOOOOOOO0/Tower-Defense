21d7e093f1a65155f3ba929a7938a1dd1bb4404c
Stephen <stevOOOOOOOOOO0@YAHOO.COM>
2019-11-15 18:59:04 -0700

    FINISHED click and drop tower functions

--- Tower Defense/Assets/Scripts/ClickBehaviour.cs
+++ Tower Defense/Assets/Scripts/ClickBehaviour.cs
@@ -2,15 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ClickBehaviour : MonoBehaviour
 {
 
-	private Vector3 mousePosition;
+	public UnityEvent ClickEvent;
+	//private Vector3 mousePosition;
 	
-	private void OnMouseDrag()
+//	private void OnMouseDrag()
+//	{
+//		mousePosition.Set(Input.mousePosition.x / Screen.width, 5f, Input.mousePosition.z / Screen.height);
+//		gameObject.transform.position = Vector3.Lerp(transform.position, mousePosition, .2f);
+//	}
+
+	public void OnMouseDown()
 	{
-		mousePosition.Set(Input.mousePosition.x / Screen.width, 5f, Input.mousePosition.z / Screen.height);
-		gameObject.transform.position = Vector3.Lerp(transform.position, mousePosition, .2f);
+		ClickEvent.Invoke();
 	}
 }
---
+++ Tower Defense/Assets/Scripts/GameObjectData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameObjectData : ScriptableObject
+{
+
+    public GameObject Value;
+
+    public void GetObject(GameObject GameObj)
+    {
+        Value = GameObj;
+    }
+
+    public void SetPosition(Transform Location)
+    {
+        Value.transform.position = Location.position;
+        Value.transform.rotation = Location.rotation;
+    }
+}
