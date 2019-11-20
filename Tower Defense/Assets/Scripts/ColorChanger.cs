a670ba7ef64b61bfd0c82dc87b946457eedcd627
514 Classroom <stevOOOOOOOOOO0@yahoo.com>
2019-11-20 14:37:45 -0700

    added Color Changer Script

---
+++ Tower Defense/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+	
+	private Color yellow;
+	private Color oldColor;
+	
+	public void ChangeToYellow()
+	{
+		oldColor = GetComponent<Renderer>().material.color;
+		yellow = Color.yellow;
+		yellow.a = .5f;
+		GetComponent<Renderer>().material.color = yellow;
+	}
+
+	public void ReturnColor()
+	{
+		GetComponent<Renderer>().material.color = oldColor;
+	}
+}
