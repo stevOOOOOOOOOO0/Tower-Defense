ba65c605221481d820e8a16f3b2b62bc49addc89
514 Classroom <stevOOOOOOOOOO0@yahoo.com>
2019-12-11 14:32:11 -0700

    work

--- Tower Defense/Assets/Scripts/TriggerBehaviour.cs
+++ Tower Defense/Assets/Scripts/TriggerBehaviour.cs
@@ -6,6 +6,8 @@ using UnityEngine.Events;
 
 public class TriggerBehaviour : MonoBehaviour
 {
+
+	public int health = 3;
 	
 	public List<Transform> Targets;
 	
@@ -29,5 +31,14 @@ public class TriggerBehaviour : MonoBehaviour
 	public void AddTarget(Collider other)
 	{
 		Targets.Add(other.transform);
+	}
+
+	public void ThreeHealthDestroy()
+	{
+		health -= 1;
+		if (health == 0)
+		{
+			gameObject.SetActive(false);
+		}
 	}
 }
